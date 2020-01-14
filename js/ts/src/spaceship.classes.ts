9d528fcb45a38091a3b240eba44e78d1e0ba6147
Nadav Sinai <nadav@algotec.co.il>
2020-01-14 15:57:01 +0200

    spaceship implementation

--- js/ts/src/spaceship.classes.ts
+++ js/ts/src/spaceship.classes.ts
@@ -1,25 +1,81 @@
 import {Complexity, Dollars, IEngine, ISpaceship, Kilograms} from './spaceship.interfaces';
 import {makeHyperDriveEngine, makeRocketWithLiquidFuel, makeRocketWithSolidFuel} from './fuel-supplies';
 
+
+
+
+
+
 export abstract class BaseSpaceShip implements ISpaceship {
-  constructor(public engine: IEngine) {
+  abstract name:string;
+  get currentLoad(): number {
+    return this._currentLoad;
   }
+
+
+
+  set currentLoad(value: Kilograms) {
+    if (value > this.maxLoad) {
+      throw new OverWeightError(`max Load of ${this.maxLoad} reached!`);
+    } else {
+      this._currentLoad = value;
+    }
+  }
+
+  abstract complexity: Complexity;
+  // tslint:disable-next-line:variable-name
+  private _currentLoad: Kilograms = 0;
+
+  abstract readonly maxLoad: Kilograms;
+  abstract readonly price: Dollars;
+
+  constructor(public engine: IEngine) {
+
+  }
+
 }
 
+
 export class Appolo extends BaseSpaceShip implements ISpaceship {
+  readonly name = 'Appolo';
+  static price = 100_000_000;
+  static complexity = Complexity.LOW;
+  readonly price = Appolo.price;
+  complexity = Appolo.complexity;
+  readonly maxLoad = 200;
+  currentLoad = 0;
+
   constructor(engine = makeRocketWithSolidFuel()) {
     super(engine);
   }
 }
 
 export class Genesis extends BaseSpaceShip implements ISpaceship {
+  readonly name = 'Genesis';
+  static price = 200_000_000;
+  static complexity = Complexity.HIGH;
+  readonly price = Genesis.price;
+  complexity = Genesis.complexity;
+  readonly maxLoad = 500;
+  currentLoad = 0;
+
   constructor(engine = makeRocketWithLiquidFuel()) {
     super(engine);
   }
+
 }
 
 export class Enterprise extends BaseSpaceShip implements ISpaceship {
+  readonly name = 'Enterprise';
+  static price = 500_000_000;
+  static complexity = Complexity.SUPER_HIGH;
+  readonly price = Enterprise.price;
+  complexity = Enterprise.complexity;
+  readonly maxLoad = 1000;
+  currentLoad = 0;
+
   constructor(engine = makeHyperDriveEngine()) {
     super(engine);
   }
+
 }
