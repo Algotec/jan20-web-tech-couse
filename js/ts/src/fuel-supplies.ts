30f6d7911b96b73c05cf9c6734234c6a87ba5528
Nadav Sinai <nadav@algotec.co.il>
2020-01-14 15:30:27 +0200

    implementation  of fuel supplies

--- js/ts/src/fuel-supplies.ts
+++ js/ts/src/fuel-supplies.ts
@@ -2,22 +2,123 @@
 import {IFuelSupply, Liters, LitersPerSecond, Octane, StartCallback, StopCallback} from './spaceship.interfaces';
 import {ModernHyperDriveEngine, RocketEngine} from './engines';
 
-//as constructor function
+// only object constructed in the scope of this const will be able to access it
+const privateMap = new WeakMap<IFuelSupply, FuelSupplyPrivateState>();
+
+export interface FuelSupplyPrivateState {
+	isPumping: boolean;
+	fuelLeft: Liters;
+	consumptionInterval?: number;
+}
+
 export function SolidFuelSupply(capacity: Liters): void {
+	this.capacity = capacity;
+	this.potency = 108 as Octane;
+	this.flow = 0.5 as LitersPerSecond;
+	this.onFuelEnd = [];
+	// set the old-school private state;
+	privateMap.set(this, {isPumping: false, fuelLeft: this.capacity});
+	//this is an old-school readonly property
+	Object.defineProperty(this, 'fuelLeft', {
+		configurable: false, enumerable: true, get(): any {
+			return privateMap.get(this).fuelLeft;
+		}
+	});
+
 }
 
 SolidFuelSupply.prototype.pump = function pump(startCb: StartCallback) {
+	// set private state
+	privateMap.get(this).isPumping = true;
+
+	// prepare the stopCallback
+	const stopCb: StopCallback = function stopCallback() {
+		privateMap.get(this).isPumping = false;
+		clearInterval(consumptionInterval);
+		return null;
+	}.bind(this);
+
+
+	// call the startCb in the next frame
+	setTimeout(() => {
+		startCb(null, stopCb);
+	});
+	const consumptionInterval = setInterval(() => {
+		if (privateMap.get(this).isPumping) {
+			if (this.fuelLeft > this.flow) {
+				privateMap.get(this).fuelLeft -= this.flow;
+			} else {
+				this.isPumping = false;
+				clearInterval(consumptionInterval); // stop the interval;
+				this.onFuelEnd.forEach(function (v: Function) {
+					v();
+				});
+			}
+		}
+	}, 1000);
+	privateMap.get(this).consumptionInterval = consumptionInterval;
+
 };
 
-// or as a class
 export class LiquidNitrogenFuelSupply implements IFuelSupply {
-	constructor(capacity: Liters) {
+	get fuelLeft(): number {
+		return this._fuelLeft;
+	}
+
+	onFuelEnd = [];
+	readonly potency: Octane = 500;
+	readonly flow = 5;
+	private _fuelLeft: Liters;
+	private consumptionInterval: number;
+	private isPumping: boolean;
+
+	constructor(public readonly capacity: Liters) {
+		this._fuelLeft = capacity;
+	}
+
+	pump(startCb: (err: (Error | null), stopCb: StopCallback) => void): void {
+		this.isPumping = true;
+		const stopCb: StopCallback = () => {
+			this.isPumping = false;
+			clearInterval(this.consumptionInterval);
+			return null;
+		};
+
+		setTimeout(() => {
+			startCb(null, stopCb);
+		});
+		// keep this timers
+		this.consumptionInterval = setInterval(() => {
+			if (this.isPumping) {
+				if (this.fuelLeft > this.flow) {
+					this._fuelLeft -= this.flow;
+				} else {
+					this.isPumping = false;
+					clearInterval(this.consumptionInterval); // stop the interval;
+					this.onFuelEnd.forEach((v) => v());
+				}
+			}
+		}, 1000);
+
 	}
 }
 
 export class NuclearFuelSupply implements IFuelSupply {
-	constructor(capacity: Liters) {
+	get fuelLeft(): number {
+		return this._fuelLeft;
 	}
+
+	onFuelEnd = [];
+	readonly potency: Octane = 10000;
+	readonly flow = 10;
+	private _fuelLeft: Liters;
+	private isPumping: boolean;
+
+	constructor(public readonly capacity: Liters) {
+		this._fuelLeft = capacity;
+	}
+
+	pump = LiquidNitrogenFuelSupply.prototype.pump.bind(this);
 }
 
 export function makeRocketWithSolidFuel() {
