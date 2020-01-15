a195d223e3010b90aafe6be6ba0eba41604f437e
Nadav Sinai <nadav@algotec.co.il>
2020-01-15 16:49:56 +0200

    drill 1 - start-  make angular image draggable

--- rxjs-stuff/src/basics.ts
+++ rxjs-stuff/src/basics.ts
@@ -6,42 +6,6 @@ const {Observable,fromEvent} = rx;
 const {filter,tap, takeUntil,} = operators;
 const img: HTMLImageElement = document.querySelector('#logo') as HTMLImageElement;
 
-const keyDown$: rx.Observable<KeyboardEvent> = fromEvent<KeyboardEvent>(document, 'keydown');
+/// drill 1 - make the angular image draggabe - hint
+// start with mouseDown event switch to a new observable of mouse move and take unill mouse up
 
-const reached$ =  new Observable(function (obs) {
-	const ob = new IntersectionObserver((entries) => {
-			entries.forEach((entry) => {
-				if (entry.intersectionRatio === 0) {
-					obs.next();
-				}
-			});
-		}
-	);
-	ob.observe(img);
-	return function () {
-		ob.disconnect();
-	};
-});
-
-keyDown$
-	.pipe(
-		filter(key => key.code.includes('Arrow')),
-		tap((keyEvent) => console.log(keyEvent)),
-		takeUntil(reached$))
-	.subscribe((keyEvent) => {
-		let val = 0;
-		switch (keyEvent.code) {
-			case 'ArrowUp':
-				val = -10;
-				break;
-			case 'ArrowDown':
-				val = +10;
-				break;
-		}
-		let top = parseInt(img.style.top.replace('px', ''), 10);
-		if (isNaN(top)) {
-			top = 0;
-		}
-		img.style.top = top + val + 'px';
-
-	});
