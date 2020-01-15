105b5aefdc4fb99d9a319da7c3fc66a3b06db022
Nadav Sinai <nadav@algotec.co.il>
2020-01-15 15:28:36 +0200

    a simple clock

--- rxjs-stuff/src/basics.ts
+++ rxjs-stuff/src/basics.ts
@@ -1,3 +1,17 @@
-import * as rx from 'rxjs'
-console.log(rx);
+import  rx from 'rxjs'
+import * as operators from 'rxjs/operators';
+// @ts-ignore
+operators= rx.operators;
+
+const pre = document.querySelector<HTMLPreElement>('#pre');
+const btn = document.querySelector<HTMLButtonElement>('#btn');
+
+rx.interval(1000,rx.animationFrameScheduler)
+	.pipe(
+		operators.map((i) => {
+			return new Date();
+		})
+	).subscribe((time) => {
+	pre.innerHTML = time.toString();
+});
 
