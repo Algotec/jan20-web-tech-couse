014c32ad7ef13a1bb6cef86b41854ab34c0656c8
Nadav Sinai <nadav@algotec.co.il>
2020-01-15 16:04:04 +0200

    deal with different speeds and reset

--- rxjs-stuff/src/counter.ts
+++ rxjs-stuff/src/counter.ts
@@ -12,20 +12,27 @@ const stopButton = document.querySelector('#stop');
 const resetButton = document.querySelector('#reset');
 const output = document.querySelector('#output');
 
-const start$ = fromEvent(startButton, 'click');
+const start$ = fromEvent(startButton, 'click').pipe(mapTo(1000));
+const half$ = fromEvent(halfButton, 'click').pipe(mapTo(500));
+const quarter$ = fromEvent(quarterButton, 'click').pipe(mapTo(250));
+const reset = () => 0;
+const reset$ = fromEvent(resetButton, 'click').pipe(mapTo(reset));
 const stop$ = fromEvent(stopButton, 'click');
 const initial = 0;
 const inc = (i: number) => i + 1;
+const starters$ = merge(start$, half$, quarter$)
+const incs$ = starters$.pipe(switchMap((time: number) => {
+	//switch map means that upon each click (previous stream value)  we start a new stream and subscribe to its result
+	// old steams are unsubscribed.
+	return interval(time)
+		.pipe(
+			mapTo(inc),
+			takeUntil(stop$)
+		);
+}));
 
-start$.pipe(switchMap((click: Event) => {
-		//switch map means that upon each click (previous stream value)  we start a new stream and subscribe to its result
-		// old steams are unsubscribed.
-		return interval(1000)
-			.pipe(mapTo(inc),
-				takeUntil(stop$)
-			)
-	}),
-	scan((acc,fn)=>fn(acc),0),
+merge(reset$, incs$).pipe(
+	scan((acc, fn) => fn(acc), initial),
 	startWith(0),
 ).subscribe((result: number) => {
 	output.innerHTML = result.toString();
