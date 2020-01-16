f7c726d3363b79b11340b3f09ba0482fe4daa00a
Nadav Sinai <nadav@algotec.co.il>
2020-01-16 22:08:31 +0200

    balance service

---
+++ src/app/shared/bank.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+import {Dollars} from '@algotec/spaceship-parts';
+
+const initialBalance = 100_000_000;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BankService {
+  get balance(): number {
+    return this._balance;
+  }
+
+  private _balance: Dollars = initialBalance;
+
+  constructor() {
+  }
+
+  deposit(sum: Dollars) {
+    this._balance += sum;
+  }
+
+  withdraw(sum: Dollars) {
+    this._balance -= sum;
+  }
+}
