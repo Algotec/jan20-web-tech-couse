dbabbfc1b3b86d5b138c3761a286f0325d6ae25e
Nadav Sinai <nadav@algotec.co.il>
2020-01-16 16:43:18 +0200

    add planets view and planet components use in modules and show how - we bind inputs to create parameterized components - aot allows for errors in template to show earlier

---
+++ src/app/planets/planets-view/planets-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planets-view',
+  templateUrl: './planets-view.component.html',
+  styleUrls: ['./planets-view.component.scss']
+})
+export class PlanetsViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/planets/planets.module.ts
+++ src/app/planets/planets.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PlanetsViewComponent } from './planets-view/planets-view.component';
+import { PlanetComponent } from './planet/planet.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PlanetsViewComponent, PlanetComponent],
+  exports:[PlanetsViewComponent],
   imports: [
     CommonModule
   ]
