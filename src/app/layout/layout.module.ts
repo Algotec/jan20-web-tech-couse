7284b839339ca312b489da45facba599baef4c46
Nadav Sinai <nadav@algotec.co.il>
2020-01-16 16:44:38 +0200

    add global styles + main component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports:[HeaderComponent],
+  declarations: [HeaderComponent, MainComponent],
+  exports:[HeaderComponent,MainComponent],
   imports: [
     CommonModule
   ]
