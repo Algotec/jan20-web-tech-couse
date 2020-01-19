489162cdbd6881aa2c87035ca7787f7cf79d462a
Nadav Sinai <nadav@algotec.co.il>
2020-01-19 15:19:58 +0200

    promises practice

---
+++ js/parctise-promises.js
@@ -0,0 +1,6 @@
+// make a function that takes in a single parameter
+// and returns a new promise. after 500 milliseconds, the promise will either
+// resolve or reject randomly with the input.
+// further down the chain - we take the value if transform it to uppercase,
+// make sure to catch all possible error and log them
+
