4748249c9e04c614947aa0e588f97d44f909cb6b
Nadav Sinai <nadav@algotec.co.il>
2020-01-21 19:01:58 +0200

    make planet droppable

--- src/app/planets/planets-view/planets-view.component.ts
+++ src/app/planets/planets-view/planets-view.component.ts
@@ -4,6 +4,7 @@ import {IPlanetData} from '../common/common.types';
 import {Observable} from 'rxjs';
 import {basePlanetPos, SpaceshipsService} from '../../spaceships/spaceships.service';
 import {map} from 'rxjs/operators';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-planets-view',
@@ -11,15 +12,34 @@ import {map} from 'rxjs/operators';
   styleUrls: ['./planets-view.component.scss']
 })
 export class PlanetsViewComponent implements OnInit {
+  boxOnPlanet: string;
   planets: Observable<IPlanetData[]> = this.planetsService.getAll();
   myShips$ = this.spaceshipsSvc.myShips$.pipe(
     map(shipWithposArr => shipWithposArr
       .filter(shipWithPos => shipWithPos.anchorPlanet === basePlanetPos.anchorPlanet) //only on earth
       .map(shipWithPos => shipWithPos.ship))); // we only need the ship
 
-  constructor(private planetsService: PlanetsService, private spaceshipsSvc: SpaceshipsService) {
+  constructor(private planetsService: PlanetsService, private spaceshipsSvc: SpaceshipsService, private router: Router) {
   }
 
+  onDrop($event: DragEvent, planet: string) {
+    $event.preventDefault();
+    let ship = $event.dataTransfer.getData("text");
+    console.log($event, planet, ship);
+    // this.router.navigate(['journey', ship, 'from', 'Earth', 'to', planet])
+  };
+
+  onDragOver($event: DragEvent, planet: string) {
+    $event.preventDefault();
+    this.boxOnPlanet = planet;
+  }
+
+  onDragLeave($event: DragEvent, planet: string) {
+    $event.preventDefault(); // notice (by  logs) that this happens too often,
+    // in less naive implementations - we would use RXjs for some throttling
+    console.log('onleave happened');
+    this.boxOnPlanet = '';
+  }
 
   ngOnInit() {
   }
