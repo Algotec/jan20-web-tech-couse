c944076a91b7c2281e6a8d06350820b4d5baa982
Nadav Sinai <nadav@algotec.co.il>
2020-02-01 23:14:05 +0200

    sync testing in service is easy

--- src/app/spaceships/spaceships.service.spec.ts
+++ src/app/spaceships/spaceships.service.spec.ts
@@ -2,6 +2,7 @@ import {TestBed} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing'
 import {SpaceshipsService} from './spaceships.service';
 import {NotificationsService} from '../notifications/notifications.service';
+import {Appolo} from '@algotec/spaceship-parts';
 
 const notificationServiceMock: Partial<NotificationsService> = {
   notify(text: string): () => (Error | null) {
@@ -10,7 +11,7 @@ const notificationServiceMock: Partial<NotificationsService> = {
 };
 
 describe('SpaceshipsService', () => {
-  let service :SpaceshipsService;
+  let service: SpaceshipsService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule.withRoutes([])],
@@ -25,8 +26,27 @@ describe('SpaceshipsService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('exposes myShips array',()=>{
+  it('exposes myShips array', () => {
     expect(Array.isArray(service.myShips)).toBeTruthy();
   });
+  it('exposes shipsAvailable$ observable which is a hashMap of string to ShipFactories', () => {
+    service.shipsAvailable$.subscribe((ships) => {
+      expect(ships[Appolo.name]).toEqual(Appolo);
+    });
+  });
+
+  it('exposes myShips$ observable, allowing subscription for myShips', () => {
+    service.myShips$.subscribe((ships) => {
+      expect(service.myShips).toEqual(ships);
+    });
+  })
+  describe('constructSpaceShip method', () => {
+    it('callled with factory it returns a promise for the new Spaceships instance', () => {
+      service.constructSpaceShip(Appolo).then(() => {
+        console.log('not done')
+      })
+    })
+
+  })
 
 });
